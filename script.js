fa1658c208fab5a1230a004fdfb9c815b9d03bd6
Jie eun Lee <jieeunlee91@gmail.com>
2022-10-10 08:08:58 -0400

    update after feedback

--- script.js
+++ script.js
@@ -13,4 +13,56 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.remove('open');
     })
 
+}) // END hamburger nav
+
+document.addEventListener("DOMContentLoaded", () => {
+    const carousel = document.querySelector('.carousel');
+
+    const prevButton = document.querySelector('#prevArrow');
+
+    const nextButton = document.querySelector('#nextArrow');
+
+    let currentSlide = 0;
+    
+    let maxSlide = slide.length - 1;
+
+    // slide.forEach((slides, index) => {
+    // slides.style.transform = `translateX(${index * 100}%)`;
+    // });
+
+    // how to click next / prev button for images to rotate
+    // 
+
+    // prevButton.addEventListener('click', (e) => {
+    //     if (currentSlide === maxSlide) {
+    //        currentSlide = 0;
+    //     } else {
+    //       currentSlide++;
+    //     }
+
+    //      slide.forEach((slides, index) => {
+    //         slides.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+    //     });
+    // })
+
+    buttons.forEach(button => {
+
+        button.addEventListener('click', () => {
+            const offset = button.galleryArrow === "#nextArrow" ? 1 : - 1
+            const slide = document.querySelector('.slide');
+            const activeSlide = slide.querySelector('active');
+            let newSlide = slide.indexOf(activeSlide) + offset;
+
+            if (newSlide < 0) newIndex = slide.children.length -1;
+            if (newSlide >= slide.children.length) newIndex = 0;
+
+            slide.children[newSlide].dataset.active = true
+            delete activeSlide.dataset.active
+            // slide.forEach((slides, index) => {
+            //     slides.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+            // });
+        })
+
+    }
+
 })
